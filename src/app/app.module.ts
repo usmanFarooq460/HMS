20152c9dd4517b44d341f91326772dd0db056263
usmanFarooq460 <faroo4609903@gmail.com>
2022-04-29 15:45:23 +0500

    My First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Accounts/login/login.component';
+import { SignupComponent } from './Accounts/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
