f816f6a9b7eff1d268f43c1215c4af36e8ac1d58
UsmanFarooq46 <usman4609903@gmail.com>
2022-06-12 19:41:06 +0500

    top bar designed

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 
+
+
 @NgModule({
   declarations: [
     TopBarComponent,
